1c84a6c4cf8f8c14938a2a569802e3f157ad8c68
Yann-ship-it <yy.iris85@gmail.com>
2023-06-05 15:05:30 +0200

    Delete post

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,24 +14,23 @@
 
             <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
                 @foreach ($posts as $post)
-                <div class="justify-center py-3 pl-3">
-                    <h2 class="text-white">{{ $post->title }}</h2>
+                <div class="justify-center py-3 pl-3 pb-5">
+                    <h2 class="text-white text-4xl font-semibold pb-1">{{ $post->title }}</h2>
 
-                    <x-link href="{{ route('posts.edit', $post) }}">Modifier mon post </x-link>
+                    <div class="flex">
+                        <x-link href="{{ route('posts.edit', $post) }}">Modifier</x-link>
 
-
-                    <x-link href="" 
-                        onclick="event.preventDefault;
-                            document.getElementById('post-destroy').submit();
-                        ">Supprimer
-                        <form action={{ route('posts.destroy', $post) }} method="post" id="post-destroy">
+                        <form class="pl-5" action={{ route('posts.destroy', $post) }} method="post" id="post-destroy">
                             @csrf
+                            <input type="hidden" name="article_id" id="article_id">
+                            <input class="items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800" 
+                            type="submit" 
+                            value="Supprimer">
                             @method('delete')
                         </form>
-                    </x-link>
-                </div> 
-                @endforeach
-            </div>
+                    </div> 
+                </div>
+            @endforeach
         </div>
     </div>
 </x-app-layout>
